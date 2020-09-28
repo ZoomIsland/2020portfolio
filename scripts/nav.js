2e4144610f8466577f240f00502b464854e8c6b7
ZoomIsland <zacherymilan@gmail.com>
2020-09-28 14:35:32 -0600

    Homescreen begun

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,14 +2,17 @@ const home = document.querySelector(".nav-home");
 const about = document.querySelector(".nav-about");
 const projects = document.querySelector(".nav-projects");
 const contact = document.querySelector(".nav-contact");
+const moreProjects = document.querySelector(".moreProjects");
 
 function navClick(element) {
   const spinner = document.querySelector(".spinner");
   const contactForm = document.querySelector(".contactContainer");
   const aboutCards = document.querySelector(".aboutCards");
+  const homeScreen = document.querySelector(".homeScreen");
   spinner.style.height = "0px";
   contactForm.style.height = "0px";
   aboutCards.style.height = "0px";
+  homeScreen.style.height = "0px";
 
   switch (element) {
     case "about":
@@ -27,7 +30,7 @@ function navClick(element) {
       contactForm.style.height = "600px";
       break;
     default:
-      alert("this works!")
+      homeScreen.style.height = "800px";
   }
 }
 
@@ -35,3 +38,4 @@ home.addEventListener("click", navClick);
 about.addEventListener("click", () => navClick("about"));
 projects.addEventListener("click", () => navClick("projects"));
 contact.addEventListener("click", () => navClick("contact"));
+moreProjects.addEventListener("click", () => navClick("projects"));
